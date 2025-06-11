9c0e145a5e00fd320ed264ffae0bbbd8dc8bfb66
GauthamPoojary01 <asthra58@gmail.com>
2025-06-11 10:16:42 +0530

    Add new feature

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 const schoolRouter = require('./routes/schoolRouter'); // 🔁 fix path here
+const form1Routes = require('./routes/form1Routes');
+
+
 
 dotenv.config();
 
@@ -17,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/users', userRoutes);
 app.use('/api/schools', schoolRouter); // ✅ this now points to schoolRouter.js
+app.use('/api/forms/form1', form1Routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
